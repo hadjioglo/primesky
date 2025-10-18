c8497a4df060a29feb59a55a7f6a38848c068665
alexandr.hadjioglo <alexandr.hadjioglo@amdaris.com>
2025-10-18 13:39:22 +0300

    implement steps for search

--- src/test/java/steps/PrimeskySteps.java
+++ src/test/java/steps/PrimeskySteps.java
@@ -2,10 +2,18 @@ package steps;
 
 import static service.PlaywrightService.loadPrimeSkyHomepage;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.datatable.DataTable;
+import service.PlaywrightService;
 
 public class PrimeskySteps {
     @Given("PrimeSky homepage is loaded")
     public void primeSkyHomepageIsLoaded() {
         loadPrimeSkyHomepage();
+    }
+
+    @When("user searches for the flight")
+    public void userSearchesForTheFlight(DataTable dataTable) {
+        PlaywrightService.searchForFlight(dataTable);
     }
 }
