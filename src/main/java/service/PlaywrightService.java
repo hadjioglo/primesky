fcdf4077d3f8ad583bd00e3f9f0884134a1af311
alexandr.hadjioglo <alexandr.hadjioglo@amdaris.com>
2025-10-18 14:16:41 +0300

    chore: automation framework updates

--- src/main/java/service/PlaywrightService.java
+++ src/main/java/service/PlaywrightService.java
@@ -5,6 +5,7 @@ import io.cucumber.datatable.DataTable;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import pages.HomePage;
+import util.ErrorHandlingUtil;
 
 /**
  * Website Explorer for https://fdev.primesky.com/
