806adb69a56831d2cb7abd69050287bd45e4faa4
alexandr.hadjioglo <alexandr.hadjioglo@amdaris.com>
2025-10-18 10:40:09 +0300

    Commit all changes for test automation update

--- src/test/java/steps/PrimeskySteps.java
+++ src/test/java/steps/PrimeskySteps.java
@@ -1,29 +1,11 @@
-
 package steps;
 
-import org.junit.jupiter.api.Assertions;
-
+import static service.PlaywrightService.loadPrimeSkyHomepage;
 import io.cucumber.java.en.Given;
-import pages.HomePage;
-import service.TestContext;
-
-import java.util.logging.Logger;
 
 public class PrimeskySteps {
-    private static final Logger logger = Logger.getLogger(PrimeskySteps.class.getName());
     @Given("PrimeSky homepage is loaded")
     public void primeSkyHomepageIsLoaded() {
-        logger.info("Navigating to PrimeSky homepage");
-        try {
-            // HomePage homePage = new HomePage(TestContext.getPage());
-            // homePage.open();
-            // Assertions.assertTrue(homePage.isOnHomePage(), "Homepage did not load successfully");
-            logger.info("PrimeSky homepage loaded successfully");
-        } catch (Exception e) {
-            logger.severe("Failed to load PrimeSky homepage: " + e.getMessage());
-            // TestContext.captureDebugScreenshot("homepage_load_failure");
-            throw new RuntimeException("Error loading homepage: " + e.getMessage());
-        }
+        loadPrimeSkyHomepage();
     }
-
 }
